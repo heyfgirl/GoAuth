1abc6dc30bba1d000c8477fa9041e0c2ce493828
heyfgirl <heyfgirl@163.com>
2020-01-15 16:35:51 +0800

    去除无关

--- main.go
+++ main.go
@@ -16,19 +16,4 @@ func main() {
 
 	router.InitRouter(app)
 	app.Run(":8080")
-
-}
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	var totnums []int = append(nums1, nums2...)
-	var num float64
-	totnums2 := []int{}
-	for i := 0; i < len(nums1); i++ {
-		for t := 0; t < len(nums2); t++ {
-			if i {
-
-			}
-		}
-	}
-
-	return num
 }
